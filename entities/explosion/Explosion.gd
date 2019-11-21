0ad08c3d473f282cc32ce16f65e234f54210b452
Ronan Duffy <duffyr32@gmail.com>
2019-11-21 17:06:01 +0000

    - Added small explosion sprites, for player bullets. - Added a short grace period for player bullets hitting their second wall.

--- entities/explosion/Explosion.gd
+++ entities/explosion/Explosion.gd
@@ -1,7 +1,9 @@
 extends AnimatedSprite
 
+var is_small : bool = false
+
 func _ready() -> void:
-	play()
+	play("small" if is_small else "big")
 
 func _on_animation_finished() -> void:
 	queue_free()
--- entities/player/bullet/PlayerBullet.gd
+++ entities/player/bullet/PlayerBullet.gd
@@ -5,12 +5,15 @@ Make sure they don't hurt you though.
 """
 extends KinematicBody2D
 
+const EXPLOSION := preload("res://entities/explosion/Explosion.tscn")
+
 export (float) var move_speed := 200.0 setget set_move_speed
 export (float) var angle := 0.0 setget set_angle
 
 var rebounded : bool = false
 
 var _velocity : Vector2
+var _die_on_rebound : bool = false
 
 func _ready() -> void:
 	_update_velocity()
@@ -18,12 +21,15 @@ func _ready() -> void:
 func _physics_process(delta : float) -> void:
 	var collision := move_and_collide(_velocity * delta)
 	if collision:
-		if rebounded:
+		if rebounded and _die_on_rebound:
+			_make_explosion()
 			queue_free()
 		else:
 			set_angle(_velocity.bounce(collision.normal).angle())
 			$Sprite.modulate = Color.red
 			rebounded = true
+			if $Timer.is_stopped():
+				$Timer.start()
 
 func set_move_speed(value : float) -> void:
 	move_speed = value
@@ -36,5 +42,14 @@ func set_angle(value : float) -> void:
 func _update_velocity() -> void:
 	_velocity = Vector2(cos(angle), sin(angle)) * move_speed
 
+func _make_explosion() -> void:
+	var explosion = EXPLOSION.instance()
+	explosion.global_position = global_position
+	explosion.is_small = true
+	get_parent().add_child(explosion)
+
 func _on_VisibilityNotifier_screen_exited() -> void:
 	queue_free()
+
+func _on_Timer_timeout() -> void:
+	_die_on_rebound = true
