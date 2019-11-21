078c5485c61a4fc582b585049b2cabec60d95e66
Ronan Duffy <duffyr32@gmail.com>
2019-11-21 19:53:40 +0000

    - Enemy bullets now do the explosion player bullets do as well.

--- entities/enemies/bullet/EnemyBullet.gd
+++ entities/enemies/bullet/EnemyBullet.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const EXPLOSION := preload("res://entities/explosion/Explosion.tscn")
+
 export (float) var move_speed := 200.0 setget set_move_speed
 export (float) var angle := 0.0 setget set_angle
 
@@ -22,8 +24,15 @@ func set_angle(value : float) -> void:
 func _update_velocity() -> void:
 	_velocity = Vector2(cos(angle), sin(angle)) * move_speed
 
+func _make_explosion() -> void:
+	var explosion = EXPLOSION.instance()
+	explosion.global_position = global_position
+	explosion.is_small = true
+	get_parent().add_child(explosion)
+
 func _on_body_entered(body : PhysicsBody2D) -> void:
 	queue_free()
+	_make_explosion()
 
 func _on_VisibilityNotifier_screen_exited() -> void:
 	queue_free()
